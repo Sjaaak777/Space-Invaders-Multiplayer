3e57218361d2215e6fc7428ca0813e4402be6bdd
Sjaak <sjaaak777@gmail.com>
2020-04-11 15:26:31 +0200

    add canvas from renderer file in engine

--- src/client/script.js
+++ src/client/script.js
@@ -1,4 +1,4 @@
-import './style.css'
 import Socket from 'socket.io-client'
+import '../engine/renderer/display'
 
 const socket = Socket('localhost:3000')
---
+++ src/engine/renderer/display.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+const canvas = document.createElement('canvas')
+const context = canvas.getContext('2d')
+canvas.width = 800
+canvas.height = 600
+
+document.body.appendChild(canvas)
