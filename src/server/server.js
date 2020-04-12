0095138e23b97b9de36420c91fd4282af16a53bd
Sjaak <sjaaak777@gmail.com>
2020-04-12 20:32:24 +0200

    added very basic multiplayer to build on

--- src/server/server.js
+++ src/server/server.js
@@ -7,14 +7,40 @@ const server = Http.createServer(app)
 const io = SocketIO(server)
 const port = 3000
 
-server.listen(port, ()=>{
-    console.log(`listening on ${port}`)
+const players = {}
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`)
 })
 
-app.get('/', (reg, res )=>{
-    res.sendFile(`${__dirname}/index.html`)
+app.get('/', (reg, res) => {
+  res.sendFile(`${__dirname}/index.html`)
 })
 
-io.on('connection', socket =>{
-    console.log(`Connection:${socket.id}`)
+io.on('connection', (socket) => {
+  socket.on('new player', () => {
+    players[socket.id] = {
+      x: 300,
+      y: 300,
+    }
+  })
+  socket.on('movement', (data) => {
+    let player = players[socket.id] || {}
+    if (data.left) {
+      player.x -= 5
+    }
+    if (data.up) {
+      player.y -= 5
+    }
+    if (data.right) {
+      player.x += 5
+    }
+    if (data.down) {
+      player.y += 5
+    }
+  })
 })
+
+setInterval(() => {
+  io.sockets.emit('state', players)
+}, 1000 / 60)
