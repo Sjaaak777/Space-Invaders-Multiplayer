ad413a1e4abaf97d0a622081bc8f0ba1c8f1f81a
Sjaak <sjaaak777@gmail.com>
2020-04-15 15:34:38 +0200

    removed test code

--- src/engine/renderer/display.js
+++ src/engine/renderer/display.js
@@ -6,12 +6,4 @@ const context = canvas.getContext('2d')
 canvas.width = 800
 canvas.height = 600
 
-const knoel = 45
-
 document.body.appendChild(canvas)
-
-let english = {
-    basic: 'Hello',
-    advanced: 'Good day there'
-}
-
