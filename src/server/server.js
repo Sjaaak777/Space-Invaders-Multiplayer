44085069fec453c73d2bb40fa51d09f0ee0966b4
Sjaak <sjaaak777@gmail.com>
2020-04-29 22:12:08 +0200

    add: adding and removing connection from players array.

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const server = Http.createServer(app)
 const io = SocketIO(server)
 const port = 3000
 
-// const players = new Players()
+const players = new Players()
 
 server.listen(port, () => {
   console.log(`listening on ${port}`)
@@ -22,8 +22,17 @@ let count = 0
 
 io.on('connect', (socket) => {
   console.log('New connection :', socket.id)
+  socket.emit('message', `Connected: ${socket.id}`)
+  players.addPlayer(socket.id)
+  players.listPlayers()
+
+
+
+  socket.emit('addPlayerToList', socket.id)
 
   socket.on('disconnect', () => {
+    io.emit('message', `Player: ${socket.id} disconnected.`)
+    players.removePlayer(socket.id)
     console.log('Connection lost:', socket.id)
   })
 
